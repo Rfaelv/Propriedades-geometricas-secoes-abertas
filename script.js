d34b3b46c5d6469c1b8f76fc22fbd1c0b3484bfe
Rafael.V <ravieira@id.uff.br>
2022-08-08 02:26:26 -0300

    Ajuste Cw

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ buttonAdd.addEventListener('click', () => {
     inputX.className = 'x'
 
     const inputY = document.createElement('input')
-    inputX.type = 'text'
-    inputX.className = 'y'
+    inputY.type = 'text'
+    inputY.className = 'y'
 
     const inputT = document.createElement('input')
-    inputX.type = 'text'
-    inputX.className = 't'
+    inputT.type = 'text'
+    inputT.className = 't'
 
     newSction.appendChild(inputX)
     newSction.appendChild(inputY)
@@ -47,6 +47,7 @@ buttonCalc.addEventListener('click', () => {
     var Ixw1 = 0
     var Iyw1 = 0
     var B = [0, 0]
+    var J = 0
 
     for (let i = 1; i < x.length; i++) {
 
@@ -78,15 +79,17 @@ buttonCalc.addEventListener('click', () => {
         Iww1 += b*t/3*(wi**2 + wj**2 + wi*wj)
 
         Ixy1 += b*t/3*(xi*yi + xj*yj + xi*yj/2 + xj*yi/2)
-        Ixw1 += b*t/3*(xi*wi + xj*wj + xi*wj/2 + xj*wi/2)
-        Iyw1 += b*t/3*(yi*wi + yj*wj + yi*wj/2 + yj*wi/2)
+        Ixw1 += b*t/3*(yi*wi + yj*wj + yi*wj/2 + yj*wi/2)
+        Iyw1 += b*t/3*(xi*wi + xj*wj + xi*wj/2 + xj*wi/2)
+
+        J += 1/3*b*t**3
         
     }
 
     console.log("Iww1 = " + Iww1)
     
-    const x01 = Iy1/A
-    const y01 = Ix1/A
+    const CGx = Iy1/A
+    const CGy = Ix1/A
     const w01 = Iw1/A
 
     // console.log("w0 = " +  w01)
@@ -101,17 +104,17 @@ buttonCalc.addEventListener('click', () => {
     var Ixy2 = 0
     var Ixw2 = 0
     var Iyw2 = 0
-    B = [B[0]-x01, B[1]-y01]
+    // B = [B[0], B[1]]
 
     for (let i = 1; i < x.length; i++) {
 
         const b = ((parseFloat(x[i].value) - parseFloat(x[i-1].value))**2 + (parseFloat(y[i].value) - parseFloat(y[i-1].value))**2)**(1/2)
 
         const t = parseFloat(tick[i-1].value)
-        const yi = parseFloat(y[i-1].value) - y01
-        const yj = parseFloat(y[i].value) - y01
-        const xi = parseFloat(x[i-1].value) - x01
-        const xj = parseFloat(x[i].value) - x01
+        const yi = parseFloat(y[i-1].value) - CGy
+        const yj = parseFloat(y[i].value) - CGy
+        const xi = parseFloat(x[i-1].value) - CGx
+        const xj = parseFloat(x[i].value) - CGx
         const wi = w1[i-1] - w01
         const wj = w1[i] - w01
 
@@ -132,20 +135,14 @@ buttonCalc.addEventListener('click', () => {
         // console.log(Ixx2)
 
         Ixy2 += b*t/3*(xi*yi + xj*yj + xi*yj/2 + xj*yi/2)
-        Ixw2 += b*t/3*(xi*wi + xj*wj + xi*wj/2 + xj*wi/2)
-        Iyw2 += b*t/3*(yi*wi + yj*wj + yi*wj/2 + yj*wi/2)
+        Ixw2 += b*t/3*(yi*wi + yj*wj + yi*wj/2 + yj*wi/2)
+        Iyw2 += b*t/3*(xi*wi + xj*wj + xi*wj/2 + xj*wi/2)
         
     }
 
-    console.log("Iww2 =" + Iww2)
-
-    const fi = Math.atan(2*Ixy2/(Ixx2-Iyy2))/2
+    const fi = Math.atan(-2*Ixy2/(Ixx2-Iyy2))/2*180/Math.PI
     const deltax = (Ixw2*Iyy2-Iyw2*Ixy2)/(Ixx2*Iyy2-Ixy2**2) 
     const deltay = (Ixw2*Ixy2-Iyw2*Ixx2)/(Ixx2*Iyy2-Ixy2**2) 
-
-    console.log('deltax = ' + deltax)
-    console.log(Ixw2/Ixx2)
-    console.log('deltay = ' + deltay)
 
     const Ixx3 = (Ixx2 + Iyy2)/2 + 1/2*((Ixx2 - Iyy2)**2 + 4*Ixy2)**(1/2)
     const Iyy3 = (Ixx2 + Iyy2)/2 - 1/2*((Ixx2 - Iyy2)**2 + 4*Ixy2)**(1/2)
@@ -157,14 +154,43 @@ buttonCalc.addEventListener('click', () => {
     const CCx3 = Math.cos(fi)*CCx2 + Math.sin(fi)*CCy2
     const CCy3 = - Math.sin(fi)*CCx2 + Math.cos(fi)*CCy2
 
+    const CGx3 = Math.cos(fi)*CGx + Math.sin(fi)*CGy
+    const CGy3 = - Math.sin(fi)*CGx + Math.cos(fi)*CGy
 
-    console.log("xg = " + x01)
-    console.log("yg = " + y01)
-    console.log("Ix = " + Ixx3)
-    console.log("Iy = " + Iyy3)
-    console.log("Cw = " + Iww3)
-    console.log("C.C = " + CCx3 + ", " + CCy3)
-    console.log("fi = "+fi)
+    const x0 = CGx3-CCx3
+    const y0 = CGy3-CCy3
+
+    const r0 = (Ixx3/A + Iyy3/A + x0**2 + y0**2)**(1/2)
+
+    const resultsOutput = document.getElementsByClassName('results')
+
+    resultsOutput[0].innerHTML = 'Área: ' + A.toFixed(2)
+    resultsOutput[1].innerHTML = 'J: ' + J.toFixed(2)
+    resultsOutput[2].innerHTML = 'Ixg: ' + Ixx2.toFixed(2)
+    resultsOutput[3].innerHTML = 'Iyg: ' + Iyy2.toFixed(2)
+    resultsOutput[4].innerHTML = 'Ixyg: ' + Ixy2.toFixed(2)
+    resultsOutput[5].innerHTML = 'xc: ' + CCx3.toFixed(2)
+    resultsOutput[6].innerHTML = 'yc: ' + CCy3.toFixed(2)
+    resultsOutput[7].innerHTML = 'Cw: ' + Iww3.toFixed(2)
+    resultsOutput[8].innerHTML = 'xg: ' + CGx.toFixed(2)
+    resultsOutput[9].innerHTML = 'yg: ' + CGy.toFixed(2)
+    resultsOutput[10].innerHTML = 'I1: ' + Ixx3.toFixed(2)
+    resultsOutput[11].innerHTML = 'I2: ' + Iyy3.toFixed(2)
+    resultsOutput[12].innerHTML = 'φ: ' + fi.toFixed(2)
+    resultsOutput[13].innerHTML = 'x0: ' + x0.toFixed(2)
+    resultsOutput[14].innerHTML = 'y0: ' + y0.toFixed(2)
+    resultsOutput[15].innerHTML = 'r0: ' + r0.toFixed(2)
+
+    document.getElementById('results').style.display = 'flex'
+
+    // console.log("A = " + A)
+    // console.log("xg = " + CGx)
+    // console.log("yg = " + CGy)
+    // console.log("Ix = " + Ixx3)
+    // console.log("Iy = " + Iyy3)
+    // console.log("Cw = " + Iww3)
+    // console.log("C.C = " + CCx3 + ", " + CCy3)
+    // console.log("fi = "+fi)
 })
 
 function calculeArea(v1, v2) {
